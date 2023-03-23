0834ac0aa319a8b1fec4cbf0f54ff173e95ed527
Oldemar Chaves <oldemar.chaves@gmail.com>
2023-03-23 21:00:35 +0200

    First release of homework LASKEYHTEEN

---
+++ README.md
@@ -0,0 +1,3 @@
+# LASKEYHTEEN
+This is a homework of the vocational school Vantaan Ammattiopisto Varia
+includes HTML, JavaScript and CSS files.
---
+++ script.js
@@ -0,0 +1,5 @@
+function summa() {
+    var num_1 = Number(document.getElementById("number-1").value);
+    var num_2 = Number(document.getElementById("number-2").value);
+    document.getElementById("summa").innerHTML = num_1 + num_2;
+}
